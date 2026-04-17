4282fef95eb958655910927a45c76bdd8a94f41d
Gaurav <gaurav.bhe.24@gmail.com>
2026-04-17 15:47:32 +0530

    bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig.js");
 
 const setupAndStartServer = async () =>  {
 
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
 
     // const PORT = 3000;
 
